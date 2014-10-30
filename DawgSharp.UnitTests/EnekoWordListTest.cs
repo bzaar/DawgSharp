50908ca8f10419106a84b1259d12c956f771519c
bzaar <slepov@gmail.com>
2014-10-30 11:52:10 +0000

    Add EnekoWordListTest.cs (issue #2)

---
+++ DawgSharp.UnitTests/EnekoWordListTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DawgSharp.UnitTests
+{
+    [TestClass]
+    public class EnekoWordListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var words = File.ReadAllLines (@"..\..\..\..\..\eneko-words.txt");
+
+            var builder = new DawgBuilder<bool> ();
+
+            foreach (var word in words)
+            {
+                builder.Insert (word, true);
+            }
+
+            builder.BuildDawg ();
+        }
+    }
+}
