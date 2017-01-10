26ffffdaba43066c20ef38eb438df6c8844a68df
Viet <bamby084@gmail.com>
2017-01-10 14:05:11 +0700

    Add new version of nuget.exe and project.json

--- DawgSharp.Nuget/build.bat
+++ DawgSharp.Nuget/build.bat
@@ -1,10 +1,7 @@
-set MSBUILD_PATH="%~1"
+echo ************************ Build the project *************************************************
 
-echo ************************ Build DawgSharp with TargetFramework .Net 4.0 ************************
-%MSBUILD_PATH% DawgSharp.msbuild /t:Clean,Build
-
-echo ************************ Build DawgSharp with TargetFramework .Net 3.5 ************************
-%MSBUILD_PATH% DawgSharp.msbuild /t:Clean,Build /p:Configuration="Release 3.5"
+dotnet restore
+dotnet build -c Release
 
 echo ************************ Create Nuget package *************************************************
 del /F /Q *.nupkg
