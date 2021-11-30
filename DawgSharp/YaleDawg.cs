bb18746e512eb89c4be493a103f0e6bf9a2fa712
Sergey Slepov <slepov@gmail.com>
2021-11-30 00:29:01 +0000

    Move YaleChild to its own file

--- DawgSharp/YaleDawg.cs
+++ DawgSharp/YaleDawg.cs
@@ -173,16 +173,4 @@ namespace DawgSharp
             }
         }
     }
-
-    readonly struct YaleChild
-    {
-        public readonly int Index;
-        public readonly ushort CharIndex;
-
-        public YaleChild(int index, ushort charIndex)
-        {
-            Index = index;
-            CharIndex = charIndex;
-        }
-    }
 }
